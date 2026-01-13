3dfc101490a7f8ad853bfb68f6a42ebc0259e5b1
Natali Lima <natalifplima@gmail.com>
2026-01-13 00:05:27 -0400

    db: add database constraints and unique index for survey responses

--- db/migrate/20260113033430_add_database_constraints.rb
+++ db/migrate/20260113033430_add_database_constraints.rb
@@ -1,10 +1,8 @@
 class AddDatabaseConstraints < ActiveRecord::Migration[8.0]
   def up
-    # NOT NULL constraints for critical fields
     change_column_null :survey_responses, :employee_id, false
     change_column_null :survey_responses, :enps, false
     
-    # Clean up duplicates before adding unique constraint
     execute <<-SQL
       DELETE FROM survey_responses
       WHERE id NOT IN (
@@ -14,7 +12,6 @@ class AddDatabaseConstraints < ActiveRecord::Migration[8.0]
       );
     SQL
     
-    # Add unique constraint for idempotent imports
     add_index :survey_responses, [:employee_id, :responded_at], 
               unique: true, 
               name: 'idx_unique_employee_response_date'
